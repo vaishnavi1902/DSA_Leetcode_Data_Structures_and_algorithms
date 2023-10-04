82464d0fa8be86f7a70ed3db72ecb55e6d92ccd4
Vaishnavi Kumbhakarna <80633315+vaishnavi1902@users.noreply.github.com>
2023-10-04 13:26:59 +0530

    Time: 1 ms (86.65%), Space: 39.7 MB (44.91%) - LeetHub

--- 0029-divide-two-integers/0029-divide-two-integers.java
+++ 0029-divide-two-integers/0029-divide-two-integers.java
@@ -3,9 +3,6 @@ class Solution {
         if(dividend == -2147483648 && divisor == -1) {
             return 2147483647; 
         }
-        float a = (float) dividend ;
-        float b = (float) divisor ;
-        int c = dividend / divisor ;
-        return c ;
+        return (int)(dividend / divisor) ;
     }
 }
