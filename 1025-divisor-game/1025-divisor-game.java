2c4b303990b0520781729415652ccf2ad5ca3fd6
Vaishnavi Kumbhakarna <80633315+vaishnavi1902@users.noreply.github.com>
2023-10-21 19:37:26 +0530

    Time: 0 ms (100.00%), Space: 39.3 MB (26.19%) - LeetHub

---
+++ 1025-divisor-game/1025-divisor-game.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean divisorGame(int n) {
+        int a = 0 , b = 0 ;
+        for(int i = 0 ; i < n-1 ; i++){
+            if(i % 2 == 0 ){
+                a++;
+            }
+            else{
+                b++;
+            }
+        }
+        if(a == b ){
+           return false ;
+        }
+        if(a == 1) {
+            return true;
+        }
+        return true;
+    }
+}
