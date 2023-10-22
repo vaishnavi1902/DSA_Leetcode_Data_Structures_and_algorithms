485f75d51532aae93a7080d800b200f4e279b159
Vaishnavi Kumbhakarna <80633315+vaishnavi1902@users.noreply.github.com>
2023-10-22 11:08:58 +0530

    1025-divisor-game.java

--- 1025-divisor-game/1025-divisor-game.java
+++ 1025-divisor-game/1025-divisor-game.java
@@ -2,19 +2,11 @@ class Solution {
     public boolean divisorGame(int n) {
         int a = 0 , b = 0 ;
         for(int i = 0 ; i < n-1 ; i++){
-            if(i % 2 == 0 ){
-                a++;
-            }
-            else{
-                b++;
-            }
+            if(i % 2 == 0 ) a++; 
+            else b++; 
         }
-        if(a == b ){
-           return false ;
-        }
-        if(a == 1) {
-            return true;
-        }
+        if(a == b) return false ; 
+        if(a == 1) return true; 
         return true;
     }
 }
