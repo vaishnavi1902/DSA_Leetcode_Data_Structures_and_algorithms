759183837d92c687caf58ce31f361e915b5bfc19
Vaishnavi Kumbhakarna <80633315+vaishnavi1902@users.noreply.github.com>
2025-01-12 12:02:38 +0530

    Time: 2 ms, Memory: 45.4 MB - LeetHub

---
+++ 15125-619-45-jump-game-ii/15125-619-45-jump-game-ii.java
@@ -0,0 +1,22 @@
+class Solution {
+  public int jump(int[] nums) {
+    int ans = 0;
+    int end = 0;
+    int farthest = 0;
+
+    // Implicit BFS
+    for (int i = 0; i < nums.length - 1; ++i) {
+      farthest = Math.max(farthest, i + nums[i]);
+      if (farthest >= nums.length - 1) {
+        ++ans;
+        break;
+      }
+      if (i == end) { 
+        ++ans;
+        end = farthest;
+      }
+    }
+
+    return ans;
+  }
+}
