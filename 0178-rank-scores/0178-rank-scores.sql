41b2dae39900bdc1d3cd89639d5966ce6184256d
Vaishnavi Kumbhakarn <80633315+vaishnavi1902@users.noreply.github.com>
2026-06-08 16:32:56 +0530

    Time: 298 ms (89.17%), Space: 0B (100%) - LeetHub

--- 0178-rank-scores/0178-rank-scores.sql
+++ 0178-rank-scores/0178-rank-scores.sql
@@ -1,4 +1,5 @@
 # Write your MySQL query statement below
+-- The DENSE_RANK() function is a SQL window function that assigns a rank to each row within a result set partition without skipping any ranking numbers when ties occur.
 SELECT score ,
        DENSE_RANK() OVER (ORDER BY score DESC) as 'rank'
 FROM Scores;
