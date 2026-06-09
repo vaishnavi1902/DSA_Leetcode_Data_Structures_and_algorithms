62566b6f577a8b306dd23335b7cb67e7c1f0b516
Vaishnavi Kumbhakarn <80633315+vaishnavi1902@users.noreply.github.com>
2026-06-09 14:12:47 +0530

    Time: 578 ms (49.44%), Space: 0B (100%) - LeetHub

--- 0180-consecutive-numbers/0180-consecutive-numbers.sql
+++ 0180-consecutive-numbers/0180-consecutive-numbers.sql
@@ -1,7 +1,17 @@
 # Write your MySQL query statement below
--- 
-SELECT DISTINCT num as ConsecutiveNums
-FROM Logs a
-WHERE num = (SELECT num from Logs WHERE id = a.id + 1) 
-      AND 
-      num = (SELECT num from Logs WHERE id = a.id + 2);
+-- Improved Improved approach
+WITH CTE AS (SELECT * ,
+       LEAD(NUM,1) OVER() AS FIRST_NUM,
+       LEAD(NUM,2) OVER() AS SECOND_NUM
+FROM Logs)
+
+SELECT DISTINCT num AS ConsecutiveNums
+FROM CTE
+WHERE num = FIRST_NUM and num = SECOND_NUM;
+
+-- My first approach
+-- SELECT DISTINCT num as ConsecutiveNums
+-- FROM Logs a
+-- WHERE num = (SELECT num from Logs WHERE id = a.id + 1) 
+--       AND 
+--       num = (SELECT num from Logs WHERE id = a.id + 2);
